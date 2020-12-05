fc3d07755b49f8a4f654f0f4b4397e88a3c0d845
ART_List <artbdarm9@mail.ru>
2020-12-05 18:38:53 +0300

    added button

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        NSLog("buttonPressed");
+    }
+    
 }
 
